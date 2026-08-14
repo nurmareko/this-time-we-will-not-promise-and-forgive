1d1cac66446d4e91768f25c8b5aa2f70cceab957
nurmareko <nurmareko@yandex.ru>
2026-08-14 22:05:15 +0700

    I wanna contribute to the chaos. I don't wanna watch and then complain.

--- irvine/serial_blink/serial_blink.ino
+++ irvine/serial_blink/serial_blink.ino
@@ -1,11 +1,17 @@
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-
+  Serial.begin(9600);
 }
 
 void loop() {
+  char c = Serial.read();
+  
+  if (c == '1' || c == '0') {
+    Serial.println(c);
+  }
+
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(100);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(2000);
+  delay(100);
 }
