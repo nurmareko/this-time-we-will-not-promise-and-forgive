214217742a0c54cb4f370bb094fd9dc9ea6321d7
nurmareko <nurmareko@yandex.ru>
2026-08-14 22:54:08 +0700

    And my nightmares will have nightmares every night

--- irvine/serial_blink/serial_blink.ino
+++ irvine/serial_blink/serial_blink.ino
@@ -5,10 +5,12 @@ void setup() {
 }
 
 void loop() {
-  char c = Serial.read();
-  if (c == '1') {
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else if (c == '0') {
-    digitalWrite(LED_BUILTIN, LOW);
+  if (Serial.available() > 0) {
+     char c = Serial.read();
+    if (c == '1') {
+      digitalWrite(LED_BUILTIN, HIGH);
+    } else if (c == '0') {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
   }
 }
