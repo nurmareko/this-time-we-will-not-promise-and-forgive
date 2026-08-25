8671c563286bd8eb64397214a27a6adedfb55dbf
nurmareko <nurmareko@yandex.ru>
2026-08-26 02:40:09 +0700

    Add auto tracking form and logout handling

--- dr_chuck/autos_db/autos.php
+++ dr_chuck/autos_db/autos.php
@@ -1,3 +1,20 @@
+<?php
+require_once 'pdo.php';
+
+if (isset($_GET['email'])) {
+    $email = $_GET['email'];
+} else {
+    die('email parameter missing');
+}
+
+if (isset($_POST['logout'])) {
+    die(header('location:index.php'));
+}
+
+?>
+
+<!----------------------------------------------------------------------------->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +23,22 @@
 	<title>Drestayumna Nurmareko</title>
 </head>
 <body>
-    <p>autos app</p>
+    <h1>Tracking Autos for <?= $email ?></h1>
+    <form action="#" method="post">
+        <label for="">
+            Make <input type="text">
+        </label>
+        <br>
+        <label for="">
+            Year <input type="text">
+        </label>
+        <br>
+        <label for="">
+            Mileage <input type="text">
+        </label>
+        <br>
+        <input type="submit" value="Add">
+        <input type="submit" name="logout" value="Logout">
+    </form>
 </body>
 </html>
