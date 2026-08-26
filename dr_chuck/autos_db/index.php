4deb99114f71e68058122801bf6b9d03506550ef
nurmareko <nurmareko@yandex.ru>
2026-08-26 23:57:19 +0700

    Add session-aware navigation to autos index

--- dr_chuck/autos_db/index.php
+++ dr_chuck/autos_db/index.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+
+
+function display_page_content() {
+    if (!isset($_SESSION['email'])) {
+        echo('<a href="login.php">Please Log In</a>');
+    } else {
+        echo('<a href="add.php">Add New Entry</a><br>');
+        echo('<a href="logout.php">Logout</a>');
+    }
+}
+?>
+
+<!----------------------------------------------------------------------------->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +23,6 @@
 </head>
 <body>
     <h1>Welcome to Autos Database</h1>
-    <a href="login.php">Please Log In</a>
+    <?php display_page_content() ?>
 </body>
 </html>
