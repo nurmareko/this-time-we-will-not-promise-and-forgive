383aa8461938d8f3da36eaaca0398f23f498d4ec
nurmareko <nurmareko@yandex.ru>
2026-08-27 00:11:22 +0700

    Display automobiles in a database table

--- dr_chuck/autos_db/index.php
+++ dr_chuck/autos_db/index.php
@@ -1,8 +1,46 @@
 <?php
+require_once 'pdo.php';
 session_start();
 
+function display_table() {
+    global $pdo;
+
+    try {
+        $stmt = $pdo->query("SELECT * FROM autos");
+        $automobiles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        error_log('Cannot retrieve data: ' . $e->getMessage());
+        die('Sorry, cannot retrieve data at the moment');
+    }
+
+    if ($automobiles === false) {
+        echo('<p>no data found!</p>');
+    } else {
+        echo("<h2>Automobiles</h2>");
+        echo("<table border='1'>");
+        echo("<tr><th>Year</th><th>Make</th><th>Mileage</th><th>Action</th></tr>");
+
+        foreach ($automobiles as $automobile) {
+            $auto_id = htmlentities($automobile['auto_id']);
+            $year = htmlentities($automobile['year']);
+            $make = htmlentities($automobile['make']);
+            $mileage = htmlentities($automobile['mileage']);
+
+            echo('<tr>');
+            echo("<td>$year</td>");
+            echo("<td>$make</td>");
+            echo("<td>$mileage</td>");
+            echo("<td><a href='edit.php?auto_id=$auto_id'>Edit</a> / "
+                . "<a href='delete.php?auto_id=$auto_id'>Delete</a></td>");
+            echo('</tr>');
+        }
+
+        echo('</table>');
+    }
+}
 
 function display_page_content() {
+    display_table();
     if (!isset($_SESSION['email'])) {
         echo('<a href="login.php">Please Log In</a>');
     } else {
