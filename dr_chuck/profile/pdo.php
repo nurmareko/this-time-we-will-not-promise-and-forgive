c8071992c25bfa007bbfb8e1ffcbd4e04173d0b3
nurmareko <nurmareko@yandex.ru>
2026-08-27 14:09:34 +0700

    Configure PDO database connection

--- dr_chuck/profile/pdo.php
+++ dr_chuck/profile/pdo.php
@@ -1,3 +1,15 @@
 <?php
+    $dsn = 'mysql:host=localhost;port=3306;dbname=misc';
+    $username = 'dev';
+    $password = 'dev';
+
+    try {
+        $pdo = new PDO($dsn, $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        error_log($e->getMessage());
+        die('sorry we cant connect to our database at the moment');
+    }
+
 
 ?>
