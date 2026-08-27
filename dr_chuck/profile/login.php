2d32ced1419b5dc3f3b2974063d15b6ce56fc5a1
nurmareko <nurmareko@yandex.ru>
2026-08-27 14:25:30 +0700

    Add login form to profile page

--- dr_chuck/profile/login.php
+++ dr_chuck/profile/login.php
@@ -6,6 +6,14 @@
 	<title>4070ffb0</title>
 </head>
 <body>
-    <p>login</p>
+    <p>Please Log In</p>
+    <form method="post">
+        <label for="email">Email <input type="text" name="email"></label>
+        <br>
+        <label for="pass">Password <input type="password" name="pass"></label>
+        <br>
+        <input type="submit" value="Login">
+        <input type="submit" name="cancel" value="Cancel">
+    </form>
 </body>
 </html>
