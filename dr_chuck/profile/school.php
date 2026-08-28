a5e664c89a3fd0be6171b29670a79375acdc917a
nurmareko <nurmareko@yandex.ru>
2026-08-29 01:10:31 +0700

    Add institution search endpoint

--- dr_chuck/profile/school.php
+++ dr_chuck/profile/school.php
@@ -8,4 +8,11 @@ if(!isset($_SESSION['user_id']) ) {
     die('Missing required parameter');
 }
 
+$stmt = $pdo->prepare('SELECT * FROM Institution WHERE name LIKE :term');
+$stmt->execute(['term' => $_GET['term'] . '%']);
+$institutions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header('content-type:application/json');
+echo(json_encode($institutions));
+
 ?>
