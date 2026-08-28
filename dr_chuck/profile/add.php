d6d6b226b261dcd45b5dd2f56ebde8e7cc5110ba
nurmareko <nurmareko@yandex.ru>
2026-08-28 16:57:07 +0700

    Add jQuery to profile pages

--- dr_chuck/profile/add.php
+++ dr_chuck/profile/add.php
@@ -68,6 +68,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>4070ffb0</title>
+    <script
+        src="https://code.jquery.com/jquery-4.0.0.slim.min.js"
+        integrity="sha256-8DGpv13HIm+5iDNWw1XqxgFB4mj+yOKFNb+tHBZOowc="
+        crossorigin="anonymous"></script>
 </head>
 <body>
     <h1>Adding Profile</h1>
--- dr_chuck/profile/view.php
+++ dr_chuck/profile/view.php
@@ -4,6 +4,10 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>4070ffb0</title>
+	<script
+        src="https://code.jquery.com/jquery-4.0.0.slim.min.js"
+        integrity="sha256-8DGpv13HIm+5iDNWw1XqxgFB4mj+yOKFNb+tHBZOowc="
+        crossorigin="anonymous"></script>
 </head>
 <body>
     <p>view page</p>
