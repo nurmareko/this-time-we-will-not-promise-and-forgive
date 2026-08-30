75fb041e5309fe1bde2fa3b2242c9a9396f7595b
nurmareko <nurmareko@yandex.ru>
2026-08-30 22:59:34 +0700

    Tender

--- primer/chapter_2/sandbox.cc
+++ primer/chapter_2/sandbox.cc
@@ -4,7 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Who goes with F\145rgus?\012" << endl;
+    // std::cin >> int input_value;
+    // int i = { 3.14 };
+    // double salary = wage = 9999.99;
+    int i = 3.14;
 
     return 0;
 }
