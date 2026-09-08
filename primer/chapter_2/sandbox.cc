af826725b8b6bcece53ae4e9abdb4525baf26c17
nurmareko <nurmareko@yandex.ru>
2026-09-08 19:06:09 +0700

    Держи мое сердце

--- primer/chapter_2/sandbox.cc
+++ primer/chapter_2/sandbox.cc
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-    int i = 1.01;
+    int i = 0, &r1 = i;
+    double d = 0, &r2 = d;
+
+    r2 = 3.14159;
+    r2 = r1;
+    i = r2;
+    r1 = d;
 
     return 0;
 }
