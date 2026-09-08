c504d6b86df618070fc3676aa4711d8139e81ab0
nurmareko <nurmareko@yandex.ru>
2026-09-08 19:24:09 +0700

    Пластиковые воспоминания

--- primer/chapter_2/sandbox.cc
+++ primer/chapter_2/sandbox.cc
@@ -4,13 +4,9 @@ using namespace std;
 
 int main()
 {
-    int i = 0, &r1 = i;
-    double d = 0, &r2 = d;
+    int i = 0;
 
-    r2 = 3.14159;
-    r2 = r1;
-    i = r2;
-    r1 = d;
+    int *ip = i;
 
     return 0;
 }
